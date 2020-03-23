df44dae17ff4ddf4e9282a78e00eef2270f59427
Dat Nguyen <Dat_T_Nguyen@live.com>
2020-03-22 21:44:38 -0400

    structure & preloader

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//   $(".minor").scrollTop($(".minor")[0].scrollHeight);
+//   $(".major").scroll(function() {
+//     $(".minor").scrollTop($(".minor")[0].scrollHeight);
+//   });
+
+//   $("section").scroll(function() {
+//     $(".minor").scrollTop(
+//       $(".minor")[0].scrollHeight - $("section").scrollTop() - 976
+//     );
+//   });
+// });
