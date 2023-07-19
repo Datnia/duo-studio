a3e5592c8c02e5244669a30ab903ff4562d2c271
Dat Nguyen <dat@duo-studio.co>
2023-07-19 14:47:04 -0400

    metaurl

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,8 +2,8 @@ module.exports = {
 	title: "Duo Studio — A Digital Design Studio in Washington DC",
 	url:
 		process.env.ELEVENTY_ENV === "development"
-			? "http://localhost:8080/"
-			: "https://duo-studio.co/",
+			? "http://localhost:8080"
+			: "https://duo-studio.co",
 	language: "en",
 	description:
 		"We are a creative collective that weaves together branding, strategy, and design to create thought-provoking digital experiences for businesses ready to make an impact.",
